9f55486c8dd4770e733df7afa62b4a45dac641f7
ihor <krylro@gmail.com>
2018-11-09 08:48:42 +0800

    update

--- resources/views/videos.blade.php
+++ resources/views/videos.blade.php
@@ -38,7 +38,7 @@
       foreach($videos as $video) {
       $i++;
     ?>
-    <div class="row @if ($i % 2 == 1) align-left @else align-right @endif" style="@if ($i == 1) padding-top:30px @endif">
+    <div class="col-sm-6" style="padding-top:30px">
       <div class="video-title col-sm-12" style="font-size:25px">{{$video->title}}</div>
       <div class="video col-sm-12" style="padding-top:20px">
         <iframe width="560" height="315" src="{{$video->link}}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
