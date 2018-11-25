f6498548831918d655163a2369402452f65dc768
ihor <krylro@gmail.com>
2018-11-25 17:34:44 +0800

    update

--- app/Console/Commands/testCommand.php
+++ app/Console/Commands/testCommand.php
@@ -42,24 +42,30 @@ class testCommand extends Command
      */
     public function handle()
     {
-      $coin = Masternode::where('id', 4)->first();
-      $rpc_user = $coin->rpc_user;
-      $rpc_password = $coin->rpc_password;
-      $rpc_port= $coin->rpc_port;
-      $rpc_ip= $coin->rpc_ip;
-      $client = new jsonRPCClient('http://'.$rpc_user.':'.$rpc_password.'@'.$rpc_ip.':'.$rpc_port.'/');
-      $to_address = 'NcBZUWBD1sGU6pE9j5Lqg3yaA9M2EBqwGT';
-      $amount = '0.001043495';
-      
-      Log::info('-------------------test----------------------------');
-      Log::info($coin);
-      Log::info($rpc_user);
-      Log::info($rpc_password);
-      Log::info($rpc_ip);
-      Log::info($rpc_port);
-      Log::info($to_address);
-      Log::info($amount);
-      $info = $client->sendtoaddress($to_address, floatval($amount));
-      Log::info($info);
+      $sale = Sale::where('id', '>', 0)->first();
+      //$admin = User::where('permission', 5)->first();
+      $admin = User::where('email', 'skyclean906@gmail.com')->first();
+      $admin->notify(new SalesNotification($sale));
+
+      // $coin = Masternode::where('id', 4)->first();
+      // $rpc_user = $coin->rpc_user;
+      // $rpc_password = $coin->rpc_password;
+      // $rpc_port= $coin->rpc_port;
+      // $rpc_ip= $coin->rpc_ip;
+      // $client = new jsonRPCClient('http://'.$rpc_user.':'.$rpc_password.'@'.$rpc_ip.':'.$rpc_port.'/');
+      // $to_address = 'NcBZUWBD1sGU6pE9j5Lqg3yaA9M2EBqwGT';
+      // $amount = '0.001043495';
+      //
+      // Log::info('-------------------test----------------------------');
+      // Log::info($coin);
+      // Log::info($rpc_user);
+      // Log::info($rpc_password);
+      // Log::info($rpc_ip);
+      // Log::info($rpc_port);
+      // Log::info($to_address);
+      // Log::info($amount);
+      // $info = $client->sendtoaddress($to_address, floatval($amount));
+      // Log::info($info);
+
     }
 }
