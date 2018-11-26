3e1d1f1a276444c113daca6cbacd69e2a482e104
ihor <krylro@gmail.com>
2018-11-27 01:24:30 +0800

    update

---
+++ resources/views/emails/adminrewardnotification.blade.php
@@ -0,0 +1,5 @@
+Hello!<br/>
+Congratulations!!!<br/>
+You have got platform profits on mncenter.online. Please check it on rewards page.<br/>
+Check here: https://admin.mncenter.online/rewards<br/>
+Regards
