b8ff4e4d239980ddf61345eec492ac4e9d7cfd66
skyclean906 <skyclean906@gmail.com>
2018-12-15 17:21:39 +0800

    update

--- app/Console/Commands/updateTransaction.php
+++ app/Console/Commands/updateTransaction.php
@@ -58,9 +58,6 @@ class updateTransaction extends Command
         $trans = $client->listtransactions("$user_id", 1000);
         if (!$trans) continue;
         foreach ($trans as $tran){
-            // $reward = Reward::where('transaction_id', $tran['txid'])->first();
-            // if (isset($reward->id)) continue;
-
             if ($tran['category'] == 'send') continue;
 
             $item = Transaction::where('transaction_hash', $tran['txid'])->where('user_id', $user_id)->where('type', 'DEPOSIT')->first();
